afde654dd57f76b09afd7ddfec9fcb84d0752bbf
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-24 12:25:55 +0000

    Task 5

---
+++ nop.c
@@ -0,0 +1,7 @@
+#include "monty.h"
+
+void handle_nop(stack_t **head, unsigned int num);
+{
+	void(head); /* suppress unused attribute head pointer */
+	void(num); /* suppress unused attribute num */
+}

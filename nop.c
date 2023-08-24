084fa1a7ca84432e5f5d8958f70332d860f5ad39
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-24 12:32:25 +0000

    Typo error removed

--- nop.c
+++ nop.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-void handle_nop(stack_t **head, unsigned int num);
+void handle_nop(stack_t **head, unsigned int num)
 {
 	void(head); /* suppress unused attribute head pointer */
 	void(num); /* suppress unused attribute num */

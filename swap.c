2f2a64a8dd47893f3251cec795453a816e322e32
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-25 14:18:43 +0000

    swap comments

--- swap.c
+++ swap.c
@@ -10,18 +10,18 @@
 
 void handle_swap_command(stack_t **head, unsigned int num)
 {
-	int t_value;
+	int t_value; /* temporary variable to be used in the swap */
 	stack_t *temp = *head;
 
-	if (temp == NULL || temp->next == NULL)
+	if (temp == NULL || temp->next == NULL) /* not two elements */
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", num);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		t_value = temp->n;
-		temp->n = temp->next->n;
-		temp->next->n = t_value;
+		t_value = temp->n; /* holds the value of the first node */
+		temp->n = temp->next->n; /*move second n to first node */
+		temp->next->n = t_value; /* move now first n to second node */
 	}
 }
